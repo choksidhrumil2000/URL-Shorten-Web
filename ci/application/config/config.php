7c4094e585e0335cb690fad021fa720703ecc832
Dhrumil Choksi <41354644+choksidhrumil2000@users.noreply.github.com>
2022-05-30 16:30:39 +0530

    config Changed

--- ci/application/config/config.php
+++ ci/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/URL-Shorten-main/ci/';
+$config['base_url']	= 'http://localhost/URL-Shorten/ci/';
 
 /*
 |--------------------------------------------------------------------------
